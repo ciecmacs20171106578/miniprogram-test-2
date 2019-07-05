77d8914a6bb1cc1308a217e7f566df0504b5d88e
ciecmacs20171106578 <43673694+ciecmacs20171106578@users.noreply.github.com>
2019-07-05 19:40:29 +0800

    .

--- pages/land/land.js
+++ pages/land/land.js
@@ -1,22 +1,18 @@
 Page({
   data: {
-    //判断小程序的API，回调，参数，组件等是否在当前版本可用。
+   
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   onLoad: function () {
     var that = this;
-    // 查看是否授权
+  
     wx.getSetting({
       success: function (res) {
         if (res.authSetting['scope.userInfo']) {
           wx.getUserInfo({
             success: function (res) {
-              //从数据库获取用户信息
-              //这里我使用queryUsreInfo函授获取信息，你要换成你的，或者不用
-              //that.queryUsreInfo();
-              //用户已经授权过
               wx.switchTab({
-                url: '/pages/index/index'
+                url: '/pages/util/util'
               })
             }
           });
@@ -26,14 +22,13 @@ Page({
   },
   bindGetUserInfo: function (e) {
     if (e.detail.userInfo) {
-      //用户按了允许授权按钮
+    
       var that = this;
-      //授权成功后，跳转进入小程序首页
+  
       wx.switchTab({
-        url: '/pages/index/index'
+        url: '/pages/util/util'
       })
     } else {
-      //用户按了拒绝按钮
       wx.showModal({
         title: '警告',
         content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!',
